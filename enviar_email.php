f3c8dac25d38ef1cbfd38e3e1bf1a670175013fc
2lineenergy <75531949+2lineenergy@users.noreply.github.com>
2023-06-04 17:33:10 +0200

    Update enviar_email.php

--- enviar_email.php
+++ enviar_email.php
@@ -1,23 +1,26 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Obtener los datos del formulario
-    $nombre = $_POST['nombre'];
-    $email = $_POST['email'];
-    $mensaje = $_POST['mensaje'];
+// Guardar los datos recibidos en variables:
+$nombre = $_POST['nombre'];
+$email = $_POST['email'];
+$asunto = $_POST['asunto'];
+$mensaje = $_POST['mensaje'];
+// Definir la dirección de destino:
+$dest = "info@2lineenergy.com"; 
+ 
+// Estas son cabeceras que se usan para evitar que el correo llegue a SPAM:
+$headers = "From: $nombre $email\r\n";
+$headers .= "X-Mailer: PHP5\n";
+$headers .= 'MIME-Version: 1.0' . "\n";
+$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+ 
+// Aqui definimos el asunto y armamos el cuerpo del mensaje
+$cuerpo = "Nombre: ".$nombre."<br>";
+$cuerpo .= "Email: ".$email."<br>";
+$cuerpo .= "Asunto: ".$asunto."<br>";
+$cuerpo .= "Mensaje: ".$mensaje;
 
-    // Configurar el correo electrónico
-    $destinatario = 'info@2lineenergy.com'; // Cambia esto con tu dirección de correo electrónico
-    $asunto = 'Nuevo mensaje de contacto';
-    $contenido = "Nombre: $nombre\n\n";
-    $contenido .= "Email: $email\n\n";
-    $contenido .= "Mensaje:\n$mensaje\n";
-
-    // Enviar el correo electrónico
-    $headers = "From: $nombre <$email>\r\n";
-    if (mail($destinatario, $asunto, $contenido, $headers)) {
-        echo "El mensaje ha sido enviado. ¡Gracias por tu contacto!";
-    } else {
-        echo "Lo sentimos, ha ocurrido un error al enviar el mensaje.";
-    }
+// Esta es una pequena validación, que solo envie el correo si todas las variables tiene algo de contenido:
+if($nombre != '' && $email != '' && $asunto != '' && $mensaje != ''){
+    mail($dest,$asunto,$cuerpo,$headers); //ENVIAR!
 }
 ?>
