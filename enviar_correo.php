48ab4b0b51215a52da272ba3a5f2a98659a60efe
2lineenergy <75531949+2lineenergy@users.noreply.github.com>
2023-06-25 13:37:07 +0200

    Add files via upload

---
+++ enviar_correo.php
@@ -0,0 +1,29 @@
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+  $nombre = $_POST['nombre'];
+  $correo = $_POST['correo'];
+  $asunto = $_POST['asunto'];
+  $mensaje = $_POST['mensaje'];
+
+  // Configura los detalles del servidor de correo
+  $servidor_correo = 'https://webmail.2lineenergy.com';
+  $puerto = 25;
+  $usuario = 'info@2lineenergy.com';
+  $contrasena = 'Generan2+';
+
+  // Construye el mensaje de correo
+  $cabeceras = "From: $nombre <$correo>\r\n";
+  $cabeceras .= "Reply-To: $correo\r\n";
+  $mensaje_correo = "Nombre: $nombre\n";
+  $mensaje_correo .= "Correo electrónico: $correo\n";
+  $mensaje_correo .= "Asunto: $asunto\n";
+  $mensaje_correo .= "Mensaje: $mensaje\n";
+
+  // Envía el correo
+  if (mail('destinatario@dominio.com', $asunto, $mensaje_correo, $cabeceras)) {
+    echo "El correo ha sido enviado correctamente.";
+  } else {
+    echo "Hubo un error al enviar el correo.";
+  }
+}
+?>
