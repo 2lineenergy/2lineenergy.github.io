6ece1ce66c531bb938369510ee61e7efca063e51
2lineenergy <75531949+2lineenergy@users.noreply.github.com>
2023-06-25 15:54:57 +0200

    Update enviar_correo.php

--- enviar_correo.php
+++ enviar_correo.php
@@ -1,29 +1,22 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-  $nombre = $_POST['nombre'];
-  $correo = $_POST['correo'];
-  $asunto = $_POST['asunto'];
-  $mensaje = $_POST['mensaje'];
+    $nombre = $_POST['nombre'];
+    $email = $_POST['email'];
+    $mensaje = $_POST['mensaje'];
 
-  // Configura los detalles del servidor de correo
-  $servidor_correo = 'http://webmail.2lineenergy.com';
-  $puerto = 587;
-  $usuario = 'info@2lineenergy.com';
-  $contrasena = 'Generan2+';
+    $to = 'info@2lineenergy.com'; // Cambia esto por la dirección de correo del destinatario
+    $subject = 'Nuevo mensaje de contacto';
+    $message = "Nombre: $nombre\n";
+    $message .= "Email: $email\n";
+    $message .= "Mensaje: $mensaje\n";
 
-  // Construye el mensaje de correo
-  $cabeceras = "From: $nombre <$correo>\r\n";
-  $cabeceras .= "Reply-To: $correo\r\n";
-  $mensaje_correo = "Nombre: $nombre\n";
-  $mensaje_correo .= "Correo electrónico: $correo\n";
-  $mensaje_correo .= "Asunto: $asunto\n";
-  $mensaje_correo .= "Mensaje: $mensaje\n";
+    $headers = "From: $email\r\n";
+    $headers .= "Reply-To: $email\r\n";
 
-  // Envía el correo
-  if (mail('info@2lineenergy.com', $asunto, $mensaje_correo, $cabeceras)) {
-    echo "El correo ha sido enviado correctamente.";
-  } else {
-    echo "Hubo un error al enviar el correo.";
-  }
+    if (mail($to, $subject, $message, $headers)) {
+        echo 'Correo electrónico enviado correctamente.';
+    } else {
+        echo 'Error al enviar el correo electrónico.';
+    }
 }
 ?>
