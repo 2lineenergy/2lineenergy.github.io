d248c60a45b5545cc20fc1e2c10ba93e1c4b81ae
2lineenergy <75531949+2lineenergy@users.noreply.github.com>
2024-11-29 20:15:58 +0100

    Update contact.js

--- mail/contact.js
+++ mail/contact.js
@@ -34,14 +34,18 @@ $(function () {
                             .append('</div>');
                     $('#contactForm').trigger("reset");
                 },
-                error: function () {
-                    $('#success').html("<div class='alert alert-danger'>");
-                    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
-                            .append("</button>");
-                    $('#success > .alert-danger').append($("<strong>").text("Sorry " + name + ", it seems that our mail server is not responding. Please try again later!"));
-                    $('#success > .alert-danger').append('</div>');
-                    $('#contactForm').trigger("reset");
-                },
+error: function (xhr) {
+    let errorMessage = "Sorry " + name + ", it seems that our mail server is not responding.";
+    if (xhr.responseJSON && xhr.responseJSON.message) {
+        errorMessage = xhr.responseJSON.message;
+    }
+    $('#success').html("<div class='alert alert-danger'>");
+    $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button>");
+    $('#success > .alert-danger').append($("<strong>").text(errorMessage));
+    $('#success > .alert-danger').append('</div>');
+    $('#contactForm').trigger("reset");
+}
+
                 complete: function () {
                     setTimeout(function () {
                         $this.prop("disabled", false);
